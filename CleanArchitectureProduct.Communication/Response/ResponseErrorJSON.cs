53b0264aac269346ff68263d4de3e2ff934a389e
Alex Junior <silva_jralex@hotmail.com>
2024-04-05 07:24:55 -0300

    first commit

---
+++ CleanArchitectureProduct.Communication/Response/ResponseErrorJSON.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CleanArchitectureProduct.Communication.Response
+{
+    public  class ResponseErrorJSON
+    {
+        public string Message { get; set; } = string.Empty;
+        public ResponseErrorJSON(string message) 
+        { 
+            Message = message;
+        }
+    }
+}
